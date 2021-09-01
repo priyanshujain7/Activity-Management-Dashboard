854491d06a0b42cc89d7076f919dcee7c2a0d3c2
Yshika Agarwal <yshika.agarwal@geminisolutions.in>
2021-09-01 19:37:36 +0530

    login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,8 +3,10 @@ import "./style.scss";
 const Login = () => {
     return (
         <div className="login-form body-bg">
-            <div className="login-card header-bg">
-                Login Fomr
+            <div className="login-section">
+                <div className="login-card header-bg">
+                    <h1>Active</h1>
+                </div>
             </div>
         </div>
     );
