62236e7a15b2257923b6e4c00f6fe71649202015
Yshika Agarwal <yshika.agarwal@geminisolutions.in>
2021-09-03 20:02:16 +0530

    header start

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Header from "../../components/Header";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return <>
+    <Header />
+  </>;
 };
 
 export default Dashboard;
