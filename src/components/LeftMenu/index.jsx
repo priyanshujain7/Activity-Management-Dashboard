a3f0c76cc93b1fed2b44dc06a10bfc3b472d6d3f
priyanshujaingem <priyanshu.jain@geminisolutions.in>
2021-09-06 20:40:57 +0530

    left menu config

--- src/components/LeftMenu/index.jsx
+++ src/components/LeftMenu/index.jsx
@@ -1,35 +1,34 @@
 import React from "react";
 import "../LeftMenu/styles.scss";
 import employees from "../../assets/images/employees.png";
+import leftMenuData from "../../utils/constants/config";
 
 const LeftMenu = () => {
   return (
-    <div className="left-menu">
-      <a className="link-prop" href="www.google.com">
-        <div className="label-group">
-          <img src={employees} alt="Link" />
-          <span className="label-text">Google</span>
-        </div>
-      </a>
-      <a className="link-prop" href="www.google.com">
-        <div className="label-group">
-          <img src={employees} alt="Link" />
-          <span className="label-text">Gemini Solutions</span>
-        </div>
-      </a>
-      <a className="link-prop" href="www.google.com">
-        <div className="label-group">
-          <img src={employees} alt="Link" />
-          <span className="label-text">Amazon</span>
-        </div>
-      </a>
-      <a className="link-prop" href="www.google.com">
-        <div className="label-group">
-          <img src={employees} alt="Link" />
-          <span className="label-text">GLA University</span>
-        </div>
-      </a>
-    </div>
+    <>
+      <div className="left-menu d-md-block d-sm-none">
+        {leftMenuData.map((item, idx) => {
+          const { href, src, label } = item || {};
+          return (
+            <a className="link-prop" href={href} target="_blank">
+              <div className="label-group">
+                <img src={src} alt="Link" />
+                <span className="label-text">{label}</span>
+              </div>
+            </a>
+          );
+        })}
+      </div>
+
+      <div className="m-left-menu d-block d-sm-none">
+        <a className="m-link-prop" href="www.google.com">
+          <div className="m-label-group">
+            <img src={employees} alt="Link" />
+            <span className="m-label-text">Google</span>
+          </div>
+        </a>
+      </div>
+    </>
   );
 };
 
