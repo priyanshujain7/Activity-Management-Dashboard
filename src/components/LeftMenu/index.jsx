b585072fffd12742c4796aaee83a20bb84fb2ac2
priyanshujaingem <priyanshu.jain@geminisolutions.in>
2021-09-06 19:33:54 +0530

    left menu data dd-view

--- src/components/LeftMenu/index.jsx
+++ src/components/LeftMenu/index.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import "../LeftMenu/styles.scss";
+import employees from "../../assets/images/employees.png";
 
 const LeftMenu = () => {
-  return <div className="left-menu"></div>;
+  return (
+    <div className="left-menu">
+      <a className="link-prop" href="www.google.com">
+        <div className="label-group">
+          <img src={employees} alt="Link" />
+          <span className="label-text">Google</span>
+        </div>
+      </a>
+      <a className="link-prop" href="www.google.com">
+        <div className="label-group">
+          <img src={employees} alt="Link" />
+          <span className="label-text">Gemini Solutions</span>
+        </div>
+      </a>
+      <a className="link-prop" href="www.google.com">
+        <div className="label-group">
+          <img src={employees} alt="Link" />
+          <span className="label-text">Amazon</span>
+        </div>
+      </a>
+      <a className="link-prop" href="www.google.com">
+        <div className="label-group">
+          <img src={employees} alt="Link" />
+          <span className="label-text">GLA University</span>
+        </div>
+      </a>
+    </div>
+  );
 };
 
 export default LeftMenu;
