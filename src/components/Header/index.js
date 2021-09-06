eebd444f0dc3dfac73ddea5259b077ca71d33a06
Yshika Agarwal <yshika.agarwal@geminisolutions.in>
2021-09-06 19:34:24 +0530

    lines hidden

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./style.scss";
 import employees from "../../assets/images/employees.png";
-import menu from "../../assets/images/menu.png"
+import menu from "../../assets/images/menu.png";
 
 const Header = () => {
   return (
     <nav className="navbar header-bg header">
-        <div className="logo">
-        <img src={menu} alt="|||" height="20px" className="image-menu "></img>
-      <h1 className="">LOGO</h1>
-        </div>
+      <div className="logo">
+        <img
+          src={menu}
+          alt="|||"
+          height="20px"
+          className="image-menu d-md-none"
+        ></img>
+        <h1 className="">LOGO</h1>
+      </div>
       <div className="heading">
         <h2> Activity Management Dashboard</h2>
       </div>
